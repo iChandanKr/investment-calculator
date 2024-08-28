1edc4b716a2fccd9171e1e464d2be016414ec0cf
Chandan Kumar <chandankumar995584@gmail.com>
2024-08-28 17:29:38 +0530

    no standalone

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,12 +1,8 @@
 import { Component,inject,Input,input} from '@angular/core';
-import type { CalculatedRestult } from '../investment-input.model';
-import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-result',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css'
 })
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,23 +1,13 @@
 import { Component, EventEmitter, output, Output, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import type { InvestmentInput } from '../investment-input.model';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  // @Output()
-  // calculate = new EventEmitter<InvestmentInput>();
-  // calculate = output<InvestmentInput>()
-  // enteredInitialInvestment = '0';
-  // enteredAnnualInvestment = '0';
-  // enteredExpectedReturn = '5';
-  // enteredDuration = '10';
   private investmentService: InvestmentService;
   constructor(investmentService: InvestmentService) {
     this.investmentService = investmentService;
